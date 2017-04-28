24577f2beb7cb0aed1c57752546fa3c8e77ff7f3
NortherKnight <northerknight@gmail.com>
2017-04-28 14:41:26 +0200

    Starting Intent not found error

---
+++ app/src/main/java/com/example/dominik/learningandroid/GitHubRepo.java
@@ -0,0 +1,14 @@
+package com.example.dominik.learningandroid;
+
+/**
+ * Created by HP on 28/04/2017.
+ */
+
+public class GitHubRepo
+{
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+}
--- app/src/main/java/com/example/dominik/learningandroid/MainActivity.java
+++ app/src/main/java/com/example/dominik/learningandroid/MainActivity.java
@@ -2,12 +2,54 @@ package com.example.dominik.learningandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    private ListView list;
+
+    public static final String URL = "http://api.github.com/";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        list = (ListView)findViewById(R.id.listView_repos);
+
+        Retrofit.Builder builder = new Retrofit.Builder().baseUrl(URL)
+                                                         .addConverterFactory(GsonConverterFactory.create());
+
+        Retrofit retrofit = builder.build();
+        GitHubClient client = retrofit.create(GitHubClient.class);
+        Call<List<GitHubRepo>> call = client.reposForUser("NortherKnight");
+
+        call.enqueue(new Callback<List<GitHubRepo>>()
+        {
+            @Override
+            public void onResponse(Call<List<GitHubRepo>> call, Response<List<GitHubRepo>> response)
+            {
+                List<GitHubRepo> repos = response.body();
+
+                list.setAdapter(new GitHubRepoAdapter(MainActivity.this, repos));
+            }
+
+            @Override
+            public void onFailure(Call<List<GitHubRepo>> call, Throwable t)
+            {
+                Toast.makeText(MainActivity.this, "error", Toast.LENGTH_SHORT);
+            }
+        });
     }
 }
